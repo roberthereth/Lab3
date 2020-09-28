5b32287993f6077b0220d592cd518a77ab2a5861
jgru-mars <59891197+jgru-mars@users.noreply.github.com>
2020-09-28 14:59:23 -0600

    Update MyGitTest.java

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,7 +2,7 @@
 public class MyGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("We both have made improvments");
+		System.out.println("We both have made improvments bacon");
 		//D:\CS-120\MyGitTest
 	}
 }
