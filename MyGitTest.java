4d8289392484f14977162b8fcb5991d08fcccf3d
Robert Hereth <rhereth@carroll.edu>
2020-09-28 14:24:38 -0600

    changed message in branch A

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,7 +2,7 @@
 public class MyGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is my GIT test!");
+		System.out.println("This is my GIT test in branch AAAA!");
 		//D:\CS-120\MyGitTest
 	}
 }
