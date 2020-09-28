b7e65297f6d4cd7c762433abd3f1e6b8bb8ad124
Robert Hereth <rhereth@carroll.edu>
2020-09-28 14:25:26 -0600

    Master changed message

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,7 +2,7 @@
 public class MyGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is my GIT test!");
+		System.out.println("This is my GIT test from master!");
 		//D:\CS-120\MyGitTest
 	}
 }
